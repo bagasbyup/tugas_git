56e069926c2ab6e4c3c294a042d33a06d0b2bd67
bagasbyup <132033329+bagasbyup@users.noreply.github.com>
2023-05-02 10:43:48 +0700

    rename to name.php
    merubah semua menjadi name.php

---
+++ name.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TUGAS GIT</title>
+    <style>
+        body {
+        background-color: grey;
+        text-align : center;
+        color : gold;
+        }
+        </style>
+</head>
+<body>
+    <?php
+    $nama = "BAGAS BAYU PRATAMA";
+    $peserta = " PESERTA BOOTCAMP IT BATCH 6";
+
+    echo "<h1> NAMA SAYA : $nama </h1></br>";
+    echo "<h3>SAYA ADALAH $peserta </h3>"; 
+    ?>
+</body>
+</html>
